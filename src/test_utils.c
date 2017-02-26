e19311a053c70d43af2504303947e54567786b66
flupe <lucas@escot.me>
2017-02-27 00:28:31 +0100

    first commit

---
+++ src/test_utils.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "utils.h"
+
+int main() {
+
+
+  // fwrite(&buf, 1, 5, fd);
+  // fseek(fd, 0, SEEK_SET);
+
+  // printf("%d\n", read_uint16_littleendian(fd));
+  // printf("%d\n", read_uint16_bigendian(fd));
+
+
+  return 0;
+}
